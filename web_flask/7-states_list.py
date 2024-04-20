b08e821f1001e66b4bc337edb7e946b802a0521c
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2024-04-20 03:18:53 +0200

    Add Sort

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,11 +12,9 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page with the states listed in alphabetical order"""
-    dict_states = storage.all(State)
-    all_states = []
-    for k, v in dict_states.items():
-        all_states.append(v)
-    return render_template('7-states_list.html', states=all_states)
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
